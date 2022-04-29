af4c45f1ca4c3389db11a83c640e39d208888530
john.wang <john.wang@bytedance.com>
2022-04-29 11:28:56 +0800

    feat: add makefile and binding header for javascript

---
+++ bindings/c/javascript.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_JAVASCRIPT_H_
+#define TREE_SITTER_JAVASCRIPT_H_
+
+#include <tree_sitter/parser.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_javascript();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // TREE_SITTER_JAVASCRIPT_H_
